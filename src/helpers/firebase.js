e6cdedf7bdbb89c1df7080214c421f679d2e79d5
vasilisa <igorevna.vasilisa@gmail.com>
2017-09-05 08:53:53 +0300

    merge + auth draft

---
+++ src/helpers/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyD7Gl8MSnXR8eYw1Az6f1E-PdhpddPBsbg",
+  authDomain: "hwyd-3d2d0.firebaseapp.com",
+  databaseURL: "https://hwyd-3d2d0.firebaseio.com",
+  projectId: "hwyd-3d2d0",
+  storageBucket: "hwyd-3d2d0.appspot.com",
+  messagingSenderId: "29725506847"
+}
+
+firebase.initializeApp(config)
+
+export const provider = new firebase.auth.GoogleAuthProvider()
+export const auth = firebase.auth()
+
+export default firebase

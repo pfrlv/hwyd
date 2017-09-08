53b84cdcad4ed60c56762224c40375f1b7283a7c
vasilisa <igorevna.vasilisa@gmail.com>
2017-09-08 21:02:19 +0300

    update footer button && firebase deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export default ({ modalRef, authRef }) => {
         <div className='footer-bottom'>
           <span className='footer-txt'>2017</span>
           <span className='footer-txt'>hwyd by Cartel</span>
-          { (authRef.user !== null) ? <span className='footer-txt footer-txt_link' onClick={authRef.logout}>Want to switch, {authRef.user.displayName}?</span> : <span className='footer-txt footer-txt_link' onClick={authRef.login}>Enter with Facebook</span> }
+          { (authRef.user !== null) ? <span className='footer-txt footer-txt_link' onClick={authRef.logout}>Logout</span> : <span className='footer-txt footer-txt_link' onClick={authRef.login}>Enter with Facebook</span> }
           <a className='footer-txt footer-txt_link footer-txt_italic' onClick={modalRef.toggle} >i</a>
         </div>
       </div>
